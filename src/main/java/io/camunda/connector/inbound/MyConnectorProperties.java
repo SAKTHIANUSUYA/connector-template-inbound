3917e6f237d201c8067ee957b49c92d46e0cce07
SAKTHIANUSUYA <113921496+SAKTHIANUSUYA@users.noreply.github.com>
2024-07-16 12:37:31 +0530

    Update MyConnectorProperties.java

--- src/main/java/io/camunda/connector/inbound/MyConnectorProperties.java
+++ src/main/java/io/camunda/connector/inbound/MyConnectorProperties.java
@@ -1,16 +1,7 @@
 package io.camunda.connector.inbound;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-
-/**
- * Configuration properties for inbound Connector
- *
- * @param sender
- * @param messagesPerMinute
- */
 public record MyConnectorProperties(
-    @NotNull String sender,
-    @Max(10) @Min(1) int messagesPerMinute
+    String eventToMonitor,
+    String directory,
+    String pollingInterval
 ) {}
